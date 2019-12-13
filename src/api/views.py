d1e52ef0a5ec0637dd2dbec87efeef9ea9b892f9
Tolga Kara <tolgakara2.0@gmail.com>
2019-12-13 14:24:07 +0100

    Forgot to add some files to git

---
+++ src/api/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+def api(request):
+    return None
+
+def timetracking_activities(request):
+    return None
+
+
---
+++ src/settings/views.py
@@ -0,0 +1,102 @@
+from django.shortcuts import render, redirect
+from src.settings.models import TimeTrackingSetting
+
+
+# Create your views here.
+def settings(request):
+    if request.user.is_authenticated:
+        # This is the only option how the user can access his settings
+        return render(request,'auth/settings/index.html',context={
+            'content':'home'
+        })
+    else:
+        return redirect('homepage')
+
+
+def settings_profile(request):
+    if request.user.is_authenticated:
+        return render(request, 'auth/settings/index.html', context={
+            'content' : 'profile'
+        })
+    else:
+        return redirect('homepage')
+
+
+def settings_timetracking(request):
+    timetracking_settings = TimeTrackingSetting.objects.get(user_id=request.user.id)
+    if request.user.is_authenticated:
+        return render(request, 'auth/settings/index.html', context={
+            'content': 'timetracking',
+            'timetracking_option': timetracking_settings.timetracking_name
+        })
+    else:
+        return redirect('homepage')
+
+
+def settings_analytics(request):
+    if request.user.is_authenticated:
+        return render(request, 'auth/settings/index.html', context={
+            'content': 'analytics'
+        })
+    else:
+        return redirect('homepage')
+
+
+def settings_projectmanagement(request):
+    if request.user.is_authenticated:
+        return render(request, 'auth/settings/index.html', context={
+            'content': 'projectmanagement'
+        })
+    else:
+        return redirect('homepage')
+
+
+def settings_team(request):
+    if request.user.is_authenticated:
+        return render(request, 'auth/settings/index.html', context={
+            'content': 'team'
+        })
+    else:
+        return redirect('homepage')
+
+
+def settings_company(request):
+    if request.user.is_authenticated:
+        return render(request, 'auth/settings/index.html', context={
+            'content': 'company'
+        })
+    else:
+        return redirect('homepage')
+
+
+def settings_authentication(request):
+    if request.user.is_authenticated:
+        return render(request, 'auth/settings/index.html', context={
+            'content': 'authentication'
+        })
+    else:
+        return redirect('homepage')
+
+
+def timetracking_post(request):
+    values = request.POST.get('timetracking_option')
+    values = [x.strip() for x in values.split(',')]
+
+    values_code = values[0]
+    values_name = values[1]
+    values_workingtime = values[2]
+    values_shortbreak = values[3]
+    values_longbreak = values[4]
+    values_cycle = values[5]
+
+    timetracking_settings = TimeTrackingSetting.objects.get(user_id=request.user.id)
+    timetracking_settings.timetracking_name = values_code
+    timetracking_settings.workingtime = values_workingtime
+    timetracking_settings.short_break = values_shortbreak
+    timetracking_settings.long_break = values_longbreak
+    timetracking_settings.cycle = values_cycle
+
+    timetracking_settings.save()
+
+
+    return redirect('settings_timetracking')
